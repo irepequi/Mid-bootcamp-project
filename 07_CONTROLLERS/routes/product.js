0b76d15e089c4a30528264b9d5198eb06d6a6d70
irepequi <irepequi@gmail.com>
2022-03-30 22:16:26 +0200

    Actualizacion 07_Controlers

--- 07_CONTROLLERS/routes/product.js
+++ 07_CONTROLLERS/routes/product.js
@@ -1,9 +1,33 @@
 var express = require("express");
+const productController = require("../controllers/productController");
+const uploadImage = require("../middleware/uploadFile");
 var router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
+// Ruta base = localhost:3000/product
+
+// localhost:3000/product/productForm/:id
+router.get("/productForm/:id", productController.viewFormProduct);
+
+// localhost:3000/product
+router.post("/:seller_id", uploadImage(), productController.addProduct);
+
+// localhost:3000/product/deleteProduct/:product_id/:seller_id
+router.get(
+  "/deleteProduct/:product_id/:seller_id",
+  productController.deleteProduct
+);
+
+// localhost:3000/product/productEditForm/:product_id
+router.get(
+  "/productEditForm/:product_id",
+  productController.viewProductEditForm
+);
+
+// localhost:3000/product/editProduct/:product_id/:seller_id
+router.post(
+  "/editProduct/:product_id/:seller_id",
+  uploadImage(),
+  productController.editProduct
+);
 
 module.exports = router;
--- 07_CONTROLLERS/routes/seller.js
+++ 07_CONTROLLERS/routes/seller.js
@@ -1,9 +1,33 @@
-var express = require('express');
+var express = require("express");
+const sellerController = require("../controllers/sellerController");
+const uploadImage = require("../middleware/uploadFile");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// Ruta base = localhost:3000/seller
+
+/* GET home page. */
+router.get("/", sellerController.viewHome);
+
+// localhost:3000/seller/register
+router.get("/register", sellerController.renderCreateForm);
+
+// localhost:3000/seller
+router.post("/", uploadImage(), sellerController.createSeller);
+
+// localhost:3000/deleteSeller/:id
+router.get("/deleteSeller/:id", sellerController.deleteSeller);
+
+// localhost:3000/seller/oneSeller/:id
+router.get("/oneSeller/:id", sellerController.viewOneSeller);
+
+// localhost:3000/product/productEditForm/:product_id
+router.get("/sellerEditForm/:seller_id", sellerController.viewSellerEditForm);
+
+// localhost:3000/product/editSeller/:product_id/:seller_id
+router.post(
+  "/editSeller/:seller_id",
+  uploadImage(),
+  sellerController.editSeller
+);
 
 module.exports = router;
